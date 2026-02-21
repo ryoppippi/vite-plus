585e042a6838739a9e0940b6d79f7e73e5a003b0
MK <fengmk2@gmail.com>
2026-02-21 09:23:20 +0800

    fix: lint errors in rolldown.config.ts

--- packages/cli/rolldown.config.ts
+++ packages/cli/rolldown.config.ts
@@ -4,9 +4,15 @@ export default defineConfig({
   input: './src/global-entry.ts',
   treeshake: false,
   external(source) {
-    if (/^node:/.test(source)) return true;
-    if (source === 'cross-spawn' || source === 'picocolors') return true;
-    if (source.includes('binding/index')) return true;
+    if (source.startsWith('node:')) {
+      return true;
+    }
+    if (source === 'cross-spawn' || source === 'picocolors') {
+      return true;
+    }
+    if (source.includes('binding/index')) {
+      return true;
+    }
     return false;
   },
   output: {
